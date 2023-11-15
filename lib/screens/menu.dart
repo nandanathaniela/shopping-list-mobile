910c2f979a6c25c5edc5996840b4db1804c70a1b
nandanathaniela <natnanda04@gmail.com>
2023-11-15 16:31:45 +0700

    tutorial08

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/widgets/left_drawer.dart';
 import 'package:shopping_list/widgets/shop_card.dart';
-
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';  
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
@@ -16,33 +11,22 @@ class MyHomePage extends StatelessWidget {
     ShopItem("Tambah Produk", Icons.add_shopping_cart),
     ShopItem("Logout", Icons.logout),
   ];
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+          child:Scaffold(
       appBar: AppBar(
         title: const Text(
           'Shopping List',
         ),
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.pink.shade900,
         foregroundColor: Colors.white,
       ),
-      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
       drawer: const LeftDrawer(),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
@@ -81,6 +65,8 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+    )
     );
+
   }
 }
